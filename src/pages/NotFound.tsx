042dced611e843e0f56ccec19b358021c4b3ecf0
M2rsh <mariuszbubak10@gmail.com>
2024-03-01 11:32:55 +0100

    meta tags some other small stuff and making 404 page more uniform with the rest

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,9 +4,9 @@ function NotFound() {
   return (
     <>
       <div className={styles.main}>
-        <h1 className={styles.maintext}>&lt;!-- 404 Not found --&gt;</h1>
+        <h1 className={styles.maintext}>404 Not found</h1>
         <p className={styles.subtext}>
-          The page you're looking for doesn't exist
+        &lt;!-- The page you're looking for doesn't exist --&gt;
         </p>
         <a className={styles.mainbutton} href="/">
           <span className={styles.smaller}>/</span> Go back{" "}
